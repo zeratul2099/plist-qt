649d08b2ba20cd42dd6436d2620cf8075b4eb2a7
zeratul2099 <zeratul2099@googlemail.com>
2011-10-01 13:11:55 +0200

    added License minor fixes

--- dialogs.py
+++ dialogs.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#       
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#       
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+# MA 02110-1301, USA.
 #
 # dialogs for plist-qt
 #
@@ -285,7 +299,8 @@ class CustomerDetailsDialog(QDialog):
         self.customer = customer
         transactions = Transaction.objects.filter(customer=customer).order_by("time").reverse()
         self.stats_image.update(transactions)
-        self.resize(self.stats_image.canvas.width(), self.stats_image.canvas.height()+300)
+        if self.stats_image.canvas:
+            self.resize(self.stats_image.canvas.width(), self.stats_image.canvas.height()+300)
         
         self.setWindowTitle(customer.name + ' details')
         self.name_field.setText(customer.name)
@@ -396,19 +411,16 @@ class StatsDialog(QDialog):
         if self.canvas:
             self.canvas.setParent(None)
             self.canvas.destroy()
-            #del self.canvas
-        self.canvas = FigureCanvasQTAgg(fig)
-        #self.stats_image = QLabel()
-        self.hide()
-        self.canvas.setParent(self.stats_image)
-        self.show()
-        #self.canvas_height = self.canvas.height()
-        #self.canvas_width = canvas.width()
-        
-        
-        
-        #self.mpl_toolbar = NavigationToolbar2QTAgg(canvas, self.stats_image)
-        self.resize(self.canvas.width(),self.canvas.height()+100)
+        try:
+            self.canvas = FigureCanvasQTAgg(fig)
+            self.hide()
+            self.canvas.setParent(self.stats_image)
+            self.show()
+
+            self.resize(self.canvas.width(),self.canvas.height()+100)
+        except AttributeError:
+            # pass if there are still no transactions
+            pass
         
     
     def update_list(self, transactions):
--- primitives.py
+++ primitives.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#       
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#       
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+# MA 02110-1301, USA.
 #
 # graphical primitives for plist-qt
 #
@@ -65,7 +79,8 @@ class NameLabel(QLabel):
     def update(self, customer):
         label = customer.name
         if customer.locked:
-            label += ' (locked)'
+            self.setTextFormat(Qt.RichText)
+            label = '<img src="img/16x16/document-encrypt.png">' + label
         self.setText(label)
 
 class DeleteButton(QPushButton):
